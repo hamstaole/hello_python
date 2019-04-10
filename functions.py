c289dac454bccef522129e24c95fc3e1a3b1fc0e
hamstaole <letaomai411998@gmail.com>
2019-04-10 18:58:53 +0200

    function again

---
+++ functions.py
@@ -0,0 +1,36 @@
+# def sayhi():
+#     print("Hello User")
+# sayhi()
+def happybirthday(person):
+     print("Happy birthday to you")
+     print("Happy birthday to you")
+     print("Happy birthday to " + person + "!")
+person = "Mai"
+happybirthday(person)
+def my_function(num):
+     return num*num*num
+print(my_function(3))
+result = my_function(4)
+print(result)
+#
+def my_list(my_num):
+     print(my_num)
+
+my_num = [10, 20, 30]
+print(my_list(my_num))
+
+# def just_return():
+#     return "Hello World"
+# print(just_return())
+
+def with_input(num):
+    print(num)
+num = [12, 34, 56, 45]
+print(num)
+def no_input(num):
+    print('Hello World')
+num = 'hallo'
+print(no_input(num))
+
+
+
