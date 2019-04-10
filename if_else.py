bed1db27ff0bdd9552216f1cf7ac3bedb8bbfe82
hamstaole <letaomai411998@gmail.com>
2019-04-10 16:44:19 +0200

    task 2

--- if_else.py
+++ if_else.py
@@ -1,8 +1,14 @@
 # ask user to enter a number. print out if the number is odd or even
 # generate a random number between 1 and 10. ask the user to guess the number and reply if the answer is true or wrong
 
-num = int(input("Enter a number : "))
+num: int = int(input("Enter a number : "))
 if num % 2 == 0:
-    print(str(num) + " is even. ")
+     print(str(num) + " is even. ")
 else:
-    print(str(num) + " is odd. ")
+     print(str(num) + " is odd. ")
+
+num = int(input("Enter a number : "))
+if 10 >= num >= 1:
+    print("True. ")
+else:
+    print("False. ")
