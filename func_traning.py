a1094902a7f46700f7c78aa0bec7919f65e07c64
hamstaole <letaomai411998@gmail.com>
2019-04-10 12:28:18 +0200

    with cli

---
+++ func_traning.py
@@ -0,0 +1,11 @@
+# function that adds two given numbers
+def add(term1, term2):
+    return term2 + term1
+print(add(3, 4))
+
+# say hello to the given name
+def greeting(person):
+     print("Say hello ")
+     print("Say hello " + person + "!")
+person = "Duong Kasino"
+greeting(person)
